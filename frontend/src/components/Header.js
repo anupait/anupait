e30c8a98a459bca0a23706fca236644bf36ec40f
anupait <anooprai790@gmail.com>
2021-05-22 21:17:26 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -25,10 +25,7 @@ const Header = () => {
             <Navbar.Brand>ProShop4(Shop that matters)</Navbar.Brand>
             <Navbar.Brand>ProShop5(Shop that matters)</Navbar.Brand>
             <Navbar.Brand>ProShop3(Shop that matters)</Navbar.Brand>
-            
-  
-            <Navbar.Brand>ProShop1(Shop that matters)</Navbar.Brand>
-            <Navbar.Brand>ProShop2(Shop that matters)</Navbar.Brand>
+           
 
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
