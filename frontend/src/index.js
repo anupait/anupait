eb58471cd93e80b8c6fe87aa0b805b4e151c5692
anupait <anooprai790@gmail.com>
2021-05-22 18:49:43 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+//this is comment from the anoop rai
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
