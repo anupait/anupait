8ff78e6fcbe56af8f5c7e17618734391d8526bd3
anupait <anooprai790@gmail.com>
2021-05-30 17:05:28 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,6 +11,8 @@ const Footer = () => {
           <Col className='text-center py-3'>Copyright1 &copy; ProShop1</Col>
           <Col className='text-center py-3'>Copyright10 &copy; ProShop2</Col>
           <Col className='text-center py-3'>Copyright11 &copy; ProShop1</Col>
+          <Col className='text-center py-3'>Copyright12 &copy; ProShop2</Col>
+          <Col className='text-center py-3'>Copyright13 &copy; ProShop1</Col>
           
         </Row>
       </Container>
