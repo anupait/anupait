dd6ce2ede41e445241fa7d119dd7a5b7f4a0eb72
anupait <anooprai790@gmail.com>
2021-06-13 13:37:16 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,25 +7,8 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
-          <Col className='text-center py-3'>Copyright2 &copy; ProShop2</Col>
-          <Col className='text-center py-3'>Copyright1 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright10 &copy; ProShop2</Col>
-          <Col className='text-center py-3'>Copyright11 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright12 &copy; ProShop2</Col>
-          <Col className='text-center py-3'>Copyright13 &copy; ProShop1</Col>
-           <Col className='text-center py-3'>Copyright14 &copy; ProShop2</Col>
-          <Col className='text-center py-3'>Copyright15 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright16 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright17 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright18 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright19 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright20 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright21 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright23 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright22 &copy; ProShop1</Col>
-    <Col className='text-center py-3'>Copyright24 &copy; ProShop1</Col>
-          <Col className='text-center py-3'>Copyright25 &copy; ProShop1</Col>
-    <Col className='text-center py-3'>Copyright26 &copy; ProShop1</Col>
+          <Col className='text-center py-3'>Copyright1 &copy; ProShop2</Col>
+          
           
         </Row>
       </Container>
