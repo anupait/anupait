438f25188df8e364666abd78d0d87fda4385e85c
anupait <anooprai790@gmail.com>
2021-07-18 14:16:01 +0530

    version 2.0

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -28,6 +28,12 @@ const Header = () => {
             <Navbar.Brand>ProShop4(Shop that matters)</Navbar.Brand>
             <Navbar.Brand>ProShop5(Shop that matters)</Navbar.Brand>
 
+            <Navbar.Brand>ProShop6(Shop that matters)</Navbar.Brand>
+            <Navbar.Brand>ProShop7(Shop that matters)</Navbar.Brand>
+            <Navbar.Brand>ProShop8(Shop that matters)</Navbar.Brand>
+            <Navbar.Brand>ProShop9(Shop that matters)</Navbar.Brand>
+            <Navbar.Brand>ProShop10(Shop that matters)</Navbar.Brand>
+
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
